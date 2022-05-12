06accb8609260752de0a7617cca23d3417b86845
S. Espinet <s.lou.espinet@gmail.com>
2022-05-12 16:47:51 -0700

    add Item Model

---
+++ Shoplister/Models/Item.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System;
+
+namespace Shoplister.Models
+{
+  public class Item
+  {
+    public int ItemId { get; set; }
+    public string Notes { get; set; }
+    public bool Checked { get; set; } = false;
+    public int StoreId { get; set; }
+
+    //public virtual ApplicationUser User { get; set; }
+
+  }
+}
