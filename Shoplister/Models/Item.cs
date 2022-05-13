fc3b69e7f99ad425a7fafe041303a5667a08c546
S. Espinet <s.lou.espinet@gmail.com>
2022-05-12 21:31:37 -0700

    update Item.cs to include name, create Item Index View

--- Shoplister/Models/Item.cs
+++ Shoplister/Models/Item.cs
@@ -6,6 +6,7 @@ namespace Shoplister.Models
   public class Item
   {
     public int ItemId { get; set; }
+    public string ItemName { get; set; }
     public string Notes { get; set; }
     public bool Checked { get; set; } = false;
     public int StoreId { get; set; }
